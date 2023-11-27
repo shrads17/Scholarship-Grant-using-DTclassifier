3c14a088cf2e57df6211ac98c00579ccf31038fb
ABHISHEK <abhiranadev@outlook.com>
2023-11-27 23:44:30 +0530

    Update Form.py

--- Form.py
+++ Form.py
@@ -109,3 +109,48 @@ if st.button('Check'):
         st.error("Not Granted")
     # st.write("option selected",option)
 
+ft = """
+<style>
+a:link , a:visited{
+color: #BFBFBF;  /* theme's text color hex code at 75 percent brightness*/
+background-color: transparent;
+text-decoration: none;
+}
+
+a:hover,  a:active {
+color: #0283C3; /* theme's primary color*/
+background-color: transparent;
+text-decoration: underline;
+}
+
+#page-container {
+  position: relative;
+  min-height: 10vh;
+}
+
+footer{
+    visibility:hidden;
+}
+
+.footer {
+position: relative;
+left:300px;
+top:230px;
+bottom: 0;
+width: 100%;
+background-color: transparent;
+color: #808080; /* theme's text color hex code at 50 percent brightness*/
+text-align: left; /* you can replace 'left' with 'center' or 'right' if you want*/
+}
+</style>
+
+<div id="page-container">
+
+<div class="footer">
+<p style='font-size: 0.875em;'>Made with <a style='display: inline; text-align:left;' href="https://streamlit.io/" target="_blank">Streamlit</a><br 'style= top:3px;'>
+with <img src="https://em-content.zobj.net/source/skype/289/red-heart_2764-fe0f.png" alt="heart" height= "10"/><a style='display: inline; text-align: left;' href="https://github.com/AbhishekRdev" target="_blank"> by Abhishek</a></p>
+</div>
+
+</div>
+"""
+st.write(ft, unsafe_allow_html=True)
